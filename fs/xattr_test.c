7a7ebe1ebada5f90a26ee6f1a5f82be60a111eee
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2025-06-18 23:03:52 +0800

    update docs

--- fs/xattr_test.c
+++ fs/xattr_test.c
@@ -15,7 +15,6 @@ int main(int argc, char *argv[]) {
     ssize_t size;
     int fd;
     
-    // create a test file
     fd = open(filepath, O_CREAT | O_RDWR, 0644);
     if (fd == -1) {
         perror("unable to create file");
@@ -26,14 +25,12 @@ int main(int argc, char *argv[]) {
     
     printf("test file created: %s\n", filepath);
     
-    // 1. set xattr
     if (setxattr(filepath, attr_name, attr_value, strlen(attr_value), 0) == -1) {
         perror("failed to set xattr");
         return 1;
     }
     printf("xattr set successfully: %s = %s\n", attr_name, attr_value);
     
-    // 2. get xattr
     size = getxattr(filepath, attr_name, buffer, sizeof(buffer));
     if (size == -1) {
         perror("failed to get xattr");
@@ -42,7 +39,6 @@ int main(int argc, char *argv[]) {
     buffer[size] = '\0';
     printf("xattr get successfully: %s = %s\n", attr_name, buffer);
     
-    // 3. list all xattrs
     size = listxattr(filepath, buffer, sizeof(buffer));
     if (size == -1) {
         perror("failed to list xattr");
@@ -61,14 +57,12 @@ int main(int argc, char *argv[]) {
         name += strlen(name) + 1;
     }
     
-    // // 4. remove xattr
     // if (removexattr(filepath, attr_name) == -1) {
     //     perror("failed to remove xattr");
     //     return 1;
     // }
     // printf("xattr removed successfully: %s\n", attr_name);
     
-    // // verify xattr has been removed
     // if (getxattr(filepath, attr_name, buffer, sizeof(buffer)) == -1) {
     //     printf("verification successful: xattr %s has been removed\n", attr_name);
     // }
